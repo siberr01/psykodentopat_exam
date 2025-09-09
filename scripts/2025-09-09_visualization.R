1715f5cc866ce3758d8db554df4e0d71e54b49ed
siberr01 <berrefjord@icloud.com>
2025-09-09 13:27:21 +0200

    creating script for visualization

---
+++ scripts/2025-09-09_visualization.R
@@ -0,0 +1,12 @@
+#----Visualization----####
+# Date: 2025-09-09
+# Author: psykodentopat
+# File name: 2025-09-09_visualization.R
+# Description: Script for visualization of exam data for psykodentopat.
+#
+#
+# Project: psykodentopat_exam.Rproj
+
+
+
+#----End----####
