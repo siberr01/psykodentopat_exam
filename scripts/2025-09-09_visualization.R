c7da3b2cb3c472558c1b1660d6ff385548977feb
siberr01 <berrefjord@icloud.com>
2025-09-09 14:47:27 +0200

    added correlation matrix, created density plot for visually inspecting age distribution across treatment. Also added t-tests for age-(treat/preOp_pain/gender).

--- scripts/2025-09-09_visualization.R
+++ scripts/2025-09-09_visualization.R
@@ -7,6 +7,37 @@
 #
 # Project: psykodentopat_exam.Rproj
 
+# Setup ----
+library(tidyverse)
+library(here)
+library(ggplot2)
+library(patchwork)
 
+# Load data
+joined_exam_data <- read_delim(here("data","joined_exam_data_2025-09-09.txt"))
+
+# Mutate from character to factor
+joined_exam_data <- joined_exam_data %>% 
+  mutate(across(where(is.character), as.factor))
+
+# Are there any correlated measures? ----
+GGally::ggcorr(joined_exam_data)
+# Seems like throatPain and swallowPain are correlated, and also age and BMI. Also seems like Patient ID is correlated with swallowPain and throatPain, probably an artifact, could also be that that the patients recruited later had more pain...?
+
+# Does the age distribution depend on treat?
+ggplot(joined_exam_data, aes(x = age, fill = treat)) +
+  geom_density(alpha = 0.5) +
+  theme_gray() +
+  labs(title = "Age distribution by treatment",
+       x = "Patient age",
+       fill = "Treatment:") +
+  theme(legend.position = "bottom") +
+  scale_fill_brewer(palette = "PuOr")
+
+
+# T-tests (Welch Two Sample t-test)
+t.test(age ~ treat, data = joined_exam_data)
+t.test(age ~ preOp_pain, data = joined_exam_data)
+t.test(age ~ gender, data = joined_exam_data)
 
 #----End----####
