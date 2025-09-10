ae9768cc170c981def0c8b4bad25c7d6e7dc3923
siberr01 <berrefjord@icloud.com>
2025-09-10 11:00:30 +0200

    added script for analysis

---
+++ scripts/2025-09-10_analysis.R
@@ -0,0 +1,12 @@
+#----Analysis----####
+# Date: 2025-09-10
+# Author: psykodentopat
+# File name: 2025-09-10_analysis.R
+# Description: R script for statistical analysis of exam data.
+#
+#
+# Project: psykodentopat_exam.Rproj
+
+
+
+#----End----####
