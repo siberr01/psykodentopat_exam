8d3f512317ac2382c9f483487fd0ac804f41e715
ingrideline <ingrid.e.holm@uit.no>
2025-09-10 11:46:45 +0200

    chi-square treatment to gender

--- scripts/2025-09-10_analysis.R
+++ scripts/2025-09-10_analysis.R
@@ -8,5 +8,15 @@
 # Project: psykodentopat_exam.Rproj
 
 
+#  Does the treatment depend on the gender of the patient?
 
+tbl <- table(joined_exam_data$gender, joined_exam_data$treat)
+tbl # 2x2 table showing how many female/male who recieved licorice/suger treatment
+
+chisq.test(tbl) # Chi-square test 
+
+# P-value is not significant, it does not seem to be a relationship between treatment and gender
+
+
+ 
 #----End----####
